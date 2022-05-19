cb977bb76d28689375a2e59a787b302bd2eabc35
Gustavo Almeida <73246160+gustavoalmei@users.noreply.github.com>
2022-05-19 06:33:34 -0300

    new repository

---
+++ main.js
@@ -0,0 +1,51 @@
+let data = document.querySelector("#timeData");
+let article = document.querySelector("#post");
+let date = new Date();
+let main = document.querySelector("main");
+
+function eventTerminal() {
+  let minimizeTerminal = document.querySelector(".mini");
+  let closeTerminal = document.querySelector(".close-icon");
+  let iconCMD = document.querySelector("#iconCMD");
+  let containerTerminal = document.querySelector("#container-terminal");
+  let terminal = document.querySelector("#terminal");
+
+  minimizeTerminal.addEventListener("click", () => {
+    containerTerminal.classList.toggle("mini-container");
+    terminal.classList.toggle("mini-terminal");
+  });
+
+  closeTerminal.addEventListener("click", () => {
+    containerTerminal.classList.add("close-container");
+    terminal.classList.add("close-terminal");
+  });
+
+  iconCMD.addEventListener("click", () => {
+    containerTerminal.classList.remove("close-container");
+    terminal.classList.remove("close-terminal");
+  });
+}
+eventTerminal();
+
+// navList()
+
+function NavList() {
+  const info = {
+    Sobre: "a",
+    Projetos: "b",
+    experiencia: "c",
+    conhecimento: "d",
+    contato: "e",
+  };
+
+  let body = document.querySelector(".body");
+
+  for (let li of list.children) {
+    li.addEventListener("click", () => {
+      let list = li.innerText;
+
+        console.log(list[info])
+    });
+  }
+}
+NavList();
